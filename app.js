7aea028601d36d10f563936513bd079de602eee4
Christopher Mitchell <76568996+CMitchell5619@users.noreply.github.com>
2021-02-15 00:17:00 -0700

    refinements

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 let peopleToHelp = 7845000000
 let peopleHelped = 0
 let energy = 100
-let metal = 100
 let totalMultiplier = 1
-let energyMultiplier = .00000000001
-let metalMultiplier = .00000000001
+let energyMultiplier = 0
 
 let helpUpgrades = {
     
     autoUpgrades: {
-        helperSubBots: {
-            price: 100,
+        tinyHelpers: {
+            price: 1500,
             quantity: 0,
-            multipler: 0,
+            multipler: 5,
             energyUse: 20,
         },
         dangerBots: {
@@ -21,11 +19,11 @@ let helpUpgrades = {
             multipler: 0,
             energyUse: 20,
         },
-        minerBots: {
-            price: 50,
+        electroBots: {
+            price: 25,
             quantity: 0,
-            multipler: 0,
-            energyUse: 20,
+            multipler: 1,
+            energyUse: 1,
         },
     },
 
@@ -33,22 +31,22 @@ let helpUpgrades = {
         helperBotSpeedServos: {
             price: 100,
             quantity: 0,
-            multipler: 5,
+            multipler: 1,
             energyUse: 2,
         },
         helperBotDangerIdentifiers: {
             price: 100,
             quantity: 0,
-            multipler: 1,
+            multipler: 50,
             energyUse: 2,
         },
     }
 }
 
 function purchaseSpeedServos() {
-    if (metal>=helpUpgrades.clickUpgrades.helperBotSpeedServos.price) {
+    if (energy>=helpUpgrades.clickUpgrades.helperBotSpeedServos.price) {
         ++helpUpgrades.clickUpgrades.helperBotSpeedServos.quantity
-        metal -= 50
+        energy -= helpUpgrades.clickUpgrades.helperBotSpeedServos.price
         totalMultiplier += helpUpgrades.clickUpgrades.helperBotSpeedServos.multipler
     }
     console.log(totalMultiplier)
@@ -56,30 +54,45 @@ function purchaseSpeedServos() {
 }
 
 function purchaseDangerDetected() {
-    if (metal>=helpUpgrades.clickUpgrades.helperBotDangerIdentifiers.price) {
+    if (energy>=helpUpgrades.clickUpgrades.helperBotDangerIdentifiers.price) {
         ++helpUpgrades.clickUpgrades.helperBotDangerIdentifiers.quantity
-        metal -= 10
+        energy -= 10
         totalMultiplier += helpUpgrades.clickUpgrades.helperBotDangerIdentifiers.multipler
     }
     console.log(totalMultiplier)
     update()
 }
 
-function purchaseMinerBots() {
-    if (metal>=helpUpgrades.autoUpgrades.minerBots.price) {
-        ++helpUpgrades.autoUpgrades.minerBots.quantity
-        metal -= 50
-        metalMultiplier += helpUpgrades.autoUpgrades.minerBots.multipler
-        energyMultiplier -= helpUpgrades.autoUpgrades.minerBots.energyUse
+function purchaseElectroBots() {
+    if (energy>=helpUpgrades.autoUpgrades.electroBots.price) {
+        ++helpUpgrades.autoUpgrades.electroBots.quantity
+        energy -= helpUpgrades.autoUpgrades.electroBots.price
+
+        energyMultiplier += helpUpgrades.autoUpgrades.electroBots.energyUse
     }
-    console.log(totalMultiplier)
-    runner()
+    update()
+}
+
+function purchaseTinyHelpers() {
+    if (energy>=helpUpgrades.autoUpgrades.tinyHelpers.price) {
+        ++helpUpgrades.autoUpgrades.tinyHelpers.quantity
+        energy -= helpUpgrades.autoUpgrades.tinyHelpers.price
+        totalMultiplier += helpUpgrades.autoUpgrades.tinyHelpers.multipler
+        energyMultiplier += helpUpgrades.autoUpgrades.tinyHelpers.energyUse
+    }
     update()
 }
 
 function runner() {
     setInterval(function(){
-         metal++; update() }, 1000);
+         energy += energyMultiplier;
+         peopleToHelp -= totalMultiplier;
+         peopleHelped += totalMultiplier;
+         update();
+         console.log(energyMultiplier);
+         energyCheck();
+        
+        }, 1000);
   }
 function myFunction() {
     setTimeout(function(){
@@ -87,11 +100,17 @@ function myFunction() {
   }
 
 function helpPeople() {
+    if (energy>0) {
+        
     peopleToHelp -= totalMultiplier
     peopleHelped += totalMultiplier
     --energy
     update()
     helpCheck(peopleToHelp)
+        
+    } else {
+        alert("Game Over")
+    }
 
 }
 
@@ -101,30 +120,29 @@ function update() {
     document.getElementById("peopleToHelpView").innerHTML = `${peopleToHelp}`;
     document.getElementById("peopleHelpedView").innerHTML = `${peopleHelped}`;
     document.getElementById("energyView").innerHTML = `${energy}`;
-    document.getElementById("metalView").innerHTML = `${metal}`;
     document.getElementById("speedServosView").innerHTML = `${helpUpgrades.clickUpgrades.helperBotSpeedServos.quantity}`;
     document.getElementById("dangerDetectorsView").innerHTML = `${helpUpgrades.clickUpgrades.helperBotDangerIdentifiers.quantity}`;
-    document.getElementById("energyProducedView").innerHTML = `${helpUpgrades.clickUpgrades.helperBotDangerIdentifiers.quantity}`;
-    document.getElementById("energyUsedView").innerHTML = `${helpUpgrades.clickUpgrades.helperBotDangerIdentifiers.quantity}`;
-    document.getElementById("totalEnergyView").innerHTML = `${helpUpgrades.clickUpgrades.helperBotDangerIdentifiers.quantity}`;
+    document.getElementById("energyProducedView").innerHTML = `${energyMultiplier}`;
+    document.getElementById("tinyBotsView").innerHTML = `${helpUpgrades.autoUpgrades.tinyHelpers.quantity}`;
+    document.getElementById("helpedPerView").innerHTML = `${totalMultiplier}`;
     
 }
 
 
 function helpCheck(people) {
-    if (people = 0) {
+    if (people <= 0) {
         
     } else {
         
     }
 }
 
-function energyCheck(energy) {
-    if (energy = 0) {
+function energyCheck() {
+    if (energy <= 0) {
+        var energy = 0
         alert('You lose')
-    } else {
-        
-    }
+    } else {}
 }
 
 update()
+runner()
